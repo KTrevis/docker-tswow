d74c1c12a6b5c1a577f32c12e0cf321054645595
KTrevis <treviskevin77@gmail.com>
2025-10-15 23:40:07 +0200

    CA MARCHE WOLLAH

--- docker/tswow-entrypoint.sh
+++ docker/tswow-entrypoint.sh
@@ -4,17 +4,20 @@ set -euo pipefail
 export NVM_DIR="/root/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
+git config --global safe.directory '*'
+
 if [ ! -d "/tswow-root/tswow-source/.git" ]; then
   echo "Cloning TSWoW sources..."
   git clone https://github.com/tswow/tswow.git --recurse /tswow-root/tswow-source
 fi
 
-if [ -f "/tswow-root/tswow-install/TrinityCore/install/trinitycore/bin/authserver" ]; then
-  echo "Build artifacts detected at $AUTH_PATH; skipping 'npm run build'."
+if [ -f "/tswow-root/tswow-install/bin/trinitycore/RelWithDebInfo/authserver" ]; then
+  echo "Build artifacts detected; skipping 'npm run build'."
 else
   echo "No build artifacts found; running 'npm run build'..."
   (cd /tswow-root/tswow-source && npm i && npm run build)
 fi
 
 cd /tswow-root/tswow-install
-tail -f
+sleep 10
+npm run start
